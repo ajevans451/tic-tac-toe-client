50b77813978c9548e2eb37dcbd6d8041577dd667
ajevans <ajecoder@gmail.com>
2020-10-15 13:27:47 -0400

    add api call + invoke function for sign up

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -1,4 +1,14 @@
 'use strict'
+const config = require('./../config')
+const store = require('./../store')
+const signUp = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    data: data
+  })
+}
 
-
-module.exports = {}
+module.exports = {
+  signUp
+}
--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -1,8 +1,14 @@
 'use strict'
 const formFields = require('./../../../lib/get-form-fields')
+const api = require('./api')
 const signUp = (event) => {
   event.preventDefault()
-  console.log('sign up firing')
+  const form = event.target
+  const data = formFields(form)
+  // console.log(data)
+  api.signUp(data)
+    .then(console.log('api signup firing'))
+    .catch(console.log('api signup not firing'))
 }
 const signIn = (event) => {
   event.preventDefault()
