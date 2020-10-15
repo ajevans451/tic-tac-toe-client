fdcafed41b5562c4e8e4decf31d34cf89f141ade
ajevans <ajecoder@gmail.com>
2020-10-15 11:48:04 -0400

    add event listeners for html forms

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,11 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
-
+const submitEvents = require('./auth/events')
 $(() => {
   // your JS code goes here
+  $('#sign-up-form').on('submit', submitEvents.signUp)
+  $('#sign-in-form').on('submit', submitEvents.signIn)
+  $('#change-password-form').on('submit', submitEvents.changePassword)
+  $('#sign-out-form').on('submit', submitEvents.signOut)
 })
