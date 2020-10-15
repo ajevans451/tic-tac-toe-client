25063f529253e43cee016fed1863294e003fb660
ajevans <ajecoder@gmail.com>
2020-10-15 11:54:05 -0400

    connect event listeners to event functions
      connected listeners in app.js to exported event functions in events.js
      successfully, when tested, all run fine

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -1,4 +1,24 @@
 'use strict'
-
-
-module.exports = {}
+const formFields = require('./../../../lib/get-form-fields')
+const signUp = (event) => {
+  event.preventDefault()
+  console.log('sign up firing')
+}
+const signIn = (event) => {
+  event.preventDefault()
+  console.log('sign in firing')
+}
+const changePassword = (event) => {
+  event.preventDefault()
+  console.log('change password firing')
+}
+const signOut = (event) => {
+  event.preventDefault()
+  console.log('sign out firing')
+}
+module.exports = {
+  signUp,
+  signIn,
+  changePassword,
+  signOut
+}
