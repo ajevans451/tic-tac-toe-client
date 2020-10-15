9d741123dbc913972b5f723c450b9b809276ca25
ajevans <ajecoder@gmail.com>
2020-10-15 15:02:14 -0400

    add api calls for signin/out/changepw

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -8,7 +8,35 @@ const signUp = function (data) {
     data: data
   })
 }
-
+const signIn = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    method: 'POST',
+    data: data
+  })
+}
+const changePW = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/change-password',
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Bearer ' + store.user.token
+    },
+    data: data
+  })
+}
+const signOut = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-out',
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Bearer ' + store.user.token
+    }
+  })
+}
 module.exports = {
-  signUp
+  signUp,
+  signIn,
+  changePW,
+  signOut
 }
