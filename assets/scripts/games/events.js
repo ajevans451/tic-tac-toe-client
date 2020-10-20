03ebf4b8544dad5d16ffbb9fc0ca5fe4842c14e0
ajevans <ajecoder@gmail.com>
2020-10-20 19:05:35 -0400

    rewrote win condition statements - working

--- assets/scripts/games/events.js
+++ assets/scripts/games/events.js
@@ -9,9 +9,9 @@ const createGame = (event) => {
   $('.box').css('background', '#F4FFF8')
   api.createGame()
     .then(ui.createGameSuccess)
-    .then(console.log(store.game.cells))
+    // .then(console.log(store.game.cells))
     .then(resetGame)
-    .then(console.log(store.game.cells))
+    // .then(console.log(store.game.cells))
     .catch(ui.createGameFailure)
 }
 const showGames = (event) => {
@@ -66,34 +66,43 @@ const gameState = function (event) {
   function checkEmpty (tile) {
     return tile !== ''
   }
-  function checkEqual (num1, num2, num3) {
+  /* function checkEqual (num1, num2, num3) {
     const equal = !!(tileArray[num1] === tileArray[num2] === tileArray[num3] && tileArray[num1] !== '')
     return equal
-  } // rows
-  if (tileArray[0] === tileArray[1] === tileArray[2] && tileArray[0] !== '') {
+  } */
+  // rows
+  if ((tileArray[0] !== '') && (tileArray[0] === tileArray[1]) && (tileArray[0] === tileArray[2])) {
+  // if (tileArray[0] === tileArray[1] === tileArray[2] && tileArray[0] !== '') {
     gameOver = true
     return gameOver
-  } else if (tileArray[3] === tileArray[4] === tileArray[5] && tileArray[3] !== '') {
+  } else if ((tileArray[3] !== '') && (tileArray[3] === tileArray[4]) && (tileArray[3] === tileArray[5])) {
+    // } else if (tileArray[3] === tileArray[4] === tileArray[5] && tileArray[3] !== '') {
     gameOver = true
     return gameOver
-  } else if (tileArray[6] === tileArray[7] === tileArray[8] && tileArray[6] !== '') {
+  } else if ((tileArray[6] !== '') && (tileArray[6] === tileArray[7]) && (tileArray[6] === tileArray[8])) {
+  // } else if (tileArray[6] === tileArray[7] === tileArray[8] && tileArray[6] !== '') {
     gameOver = true
     return gameOver
     // columns
-  } else if (tileArray[0] === tileArray[3] === tileArray[6] && tileArray[0] !== '') {
+  } else if ((tileArray[0] !== '') && (tileArray[0] === tileArray[3]) && (tileArray[0] === tileArray[6])) {
+  // } else if (tileArray[0] === tileArray[3] === tileArray[6] && tileArray[0] !== '') {
     gameOver = true
     return gameOver
-  } else if (tileArray[1] === tileArray[4] === tileArray[7] && tileArray[1] !== '') {
+  } else if ((tileArray[1] !== '') && (tileArray[1] === tileArray[4]) && (tileArray[1] === tileArray[7])) {
+  // } else if (tileArray[1] === tileArray[4] === tileArray[7] && tileArray[1] !== '') {
     gameOver = true
     return gameOver
-  } else if (tileArray[2] === tileArray[5] === tileArray[8] && tileArray[2] !== '') {
+  } else if ((tileArray[2] !== '') && (tileArray[2] === tileArray[5]) && (tileArray[2] === tileArray[8])) {
+  // } else if (tileArray[2] === tileArray[5] === tileArray[8] && tileArray[2] !== '') {
     gameOver = true
     return gameOver
     // diagonals
-  } else if (tileArray[0] === tileArray[4] === tileArray[8] && tileArray[0] !== '') {
+  } else if ((tileArray[0] !== '') && (tileArray[0] === tileArray[4]) && (tileArray[0] === tileArray[8])) {
+  // } else if (tileArray[0] === tileArray[4] === tileArray[8] && tileArray[0] !== '') {
     gameOver = true
     return gameOver
-  } else if (tileArray[2] === tileArray[4] === tileArray[6] && tileArray[2] !== '') {
+  } else if ((tileArray[2] !== '') && (tileArray[2] === tileArray[4]) && (tileArray[2] === tileArray[6])) {
+  // } else if (tileArray[2] === tileArray[4] === tileArray[6] && tileArray[2] !== '') {
     gameOver = true
     return gameOver
   } else if (tileArray.every(checkEmpty) === true) {
